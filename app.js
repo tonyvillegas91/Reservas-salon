4626cca52605c58a3b6453e30966cfeb21f10a6d
tonyvillegas91 <tony.villegas91@gmail.com>
2024-01-16 15:15:36 +0100

    update 3

--- app.js
+++ app.js
@@ -9,17 +9,20 @@ function reservar() {
     var formData = new FormData(document.getElementById('reservaForm'));
 
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://shielded-ocean-39017-ae1344eea549.herokuapp.com/procesar_reserva.php', true); // Asegúrate de que la URL sea correcta
+    xhr.open('POST', 'https://shielded-ocean-39017-ae1344eea549.herokuapp.com/procesar_reserva.php', true);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.onload = function () {
         if (xhr.status == 200) {
             try {
+                // Intenta analizar la respuesta como JSON
                 var response = JSON.parse(xhr.responseText);
                 document.getElementById('mensajeReserva').innerHTML = response;
             } catch (error) {
-                console.error("Error al procesar la respuesta JSON:", error);
+                // Si no se puede analizar como JSON, trata la respuesta como texto simple
+                document.getElementById('mensajeReserva').innerHTML = xhr.responseText;
             }
         }
     };
     xhr.send(formData);
 }
+
