f7c294e90d9778b0aa1b503a5f7282c36feebaf2
tonyvillegas91 <tony.villegas91@gmail.com>
2024-01-18 13:26:40 +0100

    update 1

--- procesar_reserva.php
+++ procesar_reserva.php
@@ -1,7 +1,7 @@
 <?php
-ini_set('display_errors', 0);
-ini_set('display_startup_errors', 0);
-error_reporting(0);
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -39,7 +39,9 @@ function enviarCorreo($nombre, $correo) {
         $mail->Timeout = 25;
 
         $mail->send();
+        error_log('Correo enviado exitosamente');
     } catch (Exception $e) {
+        error_log('Error al enviar el correo: ' . $e->getMessage());
         throw new Exception('Error al enviar el correo: ' . $e->getMessage());
     }
 }
